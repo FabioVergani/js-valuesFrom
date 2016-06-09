40a72e228c3c0bff1142ed496acc5d842191e9af
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-06-09 15:14:00 +0200

    Create valuesFrom.js

---
+++ valuesFrom.js
@@ -0,0 +1,8 @@
+function valuesFrom(obj){
+ const o=obj,m=Object.keys(o),l=m.length;
+ for(let i=0;i<l;++i){m[i]=o[m[i]];};
+ return m;
+};
+
+//test:
+console.dir(valuesFrom({a:1,b:2,c:3}));//[1, 2, 3]
